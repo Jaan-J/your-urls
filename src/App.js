43bae5ea2aa2c1e762b2514d114f98402c9e470e
Jaan-J <jaanmbjaved@gmail.com>
2022-03-02 18:01:46 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <p>
+      Edit <code>src/App.js</code> and save to reload.
+    </p>
+
+  );
+}
+
+export default App;
