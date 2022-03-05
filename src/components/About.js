4fb870c94031ff8f7be0558c1bed518ec846b01f
Jaan-J <jaanmbjaved@gmail.com>
2022-03-04 19:57:10 -0700

    Update about.js
    added footer

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,11 @@
 import NavComponent from "./Navbar";
+import Footer from "./Footer";
 function About() {
     return (
         <div>
             <NavComponent />
             <p>About</p>
+            <Footer />
         </div>
     );
 }
