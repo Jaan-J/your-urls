4fd23a4384ca81849c62f2b32a745591eba71949
Jaan-J <jaanmbjaved@gmail.com>
2022-03-04 19:57:20 -0700

    updated footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,35 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import { Nav, Navbar } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { BsFillPinMapFill, BsTelephone } from "react-icons/bs";
+import { AiTwotoneMail } from "react-icons/ai";
 import logo from '../imgs/logowhite.png';
 
 function Footer() {
     return (
-        <div className="Row">
-            <div>
-                <Navbar bg="dark" variant="dark" sticky="bottom" expand="sm" collapseOnSelect>
-                    <Navbar.Brand id="logo">
-                        <img src={logo} alt="logo" width="75px" height="75px" />
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                    <Navbar.Collapse>
-                        <Nav>
-                            <Nav.Link href='/'>Home</Nav.Link>
-                            <Nav.Link href='/About'>About Us</Nav.Link>
-                            <Nav.Link href='/Contact'>Contact</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
+
+        <footer className='bg-dark text-white pt-1 pb-1'>
+            <div className='container text-center text-md-left'>
+                <div className='row text-center text-md-left'>
+                    <div className='col-md-3 col-lg-3 col-xl-3 mx-auto mt-3'>
+                        <img src={logo} alt="logo" width="50 px" height="50px" />
+                    </div>
+                    <div className='col-md-2 col-lg-2 col-xl-2 mx-auto mt-3'>
+                        <BsTelephone />
+                        <p>647-807-5226</p>
+                    </div>
+                    <div className='col-md-3 col-lg-2 col-xl-2 mx-auto mt-3'>
+                        <AiTwotoneMail />
+                        <p>yoururlsca@gmail.com</p>
+                    </div>
+                    <div className='col-md-4 col-lg-3 col-xl-3 mx-auto mt-3'>
+                        <BsFillPinMapFill />
+                        <p>Mississauga, ON</p>
+                    </div>
+                </div>
+
             </div>
-        </div>
+
+        </footer>
+
     );
 }
 export default Footer;
